829c12a8683af78e297a3d3ba04d8cbc65499020
Ayush Singhal <ayushsinghals02@gmail.com>
2024-07-29 13:59:46 +0530

    Working Successfully

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "shader-backend",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
@@ -29,7 +29,7 @@ let package = Package(
                 .product(name: "XCTVapor", package: "vapor"),
             ],
             swiftSettings: swiftSettings
-        )
+        ),
     ]
 )
 
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,61 @@
+@preconcurrency
 import Vapor
 
+func getDocumentsDirectory() -> URL {
+    return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+}
+
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    let k = getDocumentsDirectory()
+    print("Home Directory: \(k)")
+
+    app.webSocket { _, ws in
+        ws.onText { _, text in
+            guard let data = text.data(using: .utf8) else { return }
+
+            FileManager.default.createFile(
+                atPath: k.appendingPathComponent("/shader.metal").path,
+                contents: data,
+                attributes: nil
+            )
+
+            do {
+                try shell([
+                    "-sdk",
+                    "macosx",
+                    "metal",
+                    "-c",
+                    "-o",
+                    "shader.ir",
+                    "shader.metal"
+                ])
+
+                try shell([
+                    "-sdk",
+                    "macosx",
+                    "metallib",
+                    "-o",
+                    "shader.metallib",
+                    "shader.ir"
+                ])
+
+                let url = k.appendingPathComponent("/shader.metallib")
+                if let data = try? Data(contentsOf: url) {
+                    ws.send(data)
+                }
+            }
+            catch {
+                ws.send("Error: \(error)")
+            }
+        }
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    @Sendable func shell(_ args: [String]) throws {
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/bin/xcrun")
+        task.currentDirectoryURL = k
+        task.arguments = args
+        try task.run()
+        task.waitUntilExit()
     }
 }
